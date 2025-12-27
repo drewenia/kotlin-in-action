82872a2f16e82a972f143cea867e2f543ff483bf
Cihangir Kuce <developer@Developer-machine.local>
2025-12-27 17:31:00 +0300

    2.3.7 md file olarak kaydedildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,19 +1,16 @@
-
-
 interface Expr
 class Num(val value: Int) : Expr
 class Sum(val left: Expr, val right: Expr) : Expr
 class Sub(val left: Expr, val right: Expr) : Expr
 
-fun eval(e: Expr): Int {
-    if (e is Num) return e.value
-    if (e is Sum) return eval(e.left) + eval(e.right)
-    if (e is Sub) return eval(e.left) - eval(e.right)
-    throw IllegalArgumentException("Unknown expression")
-}
+fun eval(e: Expr): Int =
+    when(e) {
+        is Num -> e.value
+        is Sum -> eval(e.left) + eval(e.right)
+        else -> throw IllegalArgumentException("Unknown expression")
+    }
 
 fun main() {
-    // (1+2) + 4
     val result = eval(
         Sum(
             Sum(Num(1), Num(2)),
