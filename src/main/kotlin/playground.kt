5b24b5ad391a770f64a28b24dc7de3f55cd65e11
Cihangir Kuce <developer@Developer-machine.local>
2025-12-27 18:27:50 +0300

    2.4.2 md file olarak kaydedildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,35 +1,9 @@
-interface Expr
-class Num(val value: Int) : Expr
-class Sum(val left: Expr, val right: Expr) : Expr
-class Sub(val left: Expr, val right: Expr) : Expr
-
-fun eval(e: Expr) : Int =
-    when(e){
-        is Num -> {
-            println("num : ${e.value}")
-            e.value
-        }
-        is Sum -> {
-            val left = eval(e.left)
-            val right = eval(e.right)
-            println("Left : $left, Right : $right")
-            left + right
-        }
-        else -> throw IllegalArgumentException("wrong argument")
-    }
-
 fun main() {
-    val result = eval(
-        Sum(
-            Sum(Num(1), Num(2)),
-            Num(4)
-        )
-    )
-    println(result)
-    // num : 1
-    // num : 2
-    // Left : 1, Right : 2
-    // num : 4
-    // Left : 3, Right : 4
-    // 7
+    downToExample()
+}
+
+fun downToExample(){
+    for (i in 100 downTo 1 step 2){
+        println(i) // 100 98 96 94 92 90 ... 2
+    }
 }
