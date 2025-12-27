36adc27fbea711e69e5f0b34bc5111002624e25b
Cihangir Kuce <developer@Developer-machine.local>
2025-12-27 17:05:09 +0300

    2.3.6 md file olarak kaydedildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,32 +1,24 @@
-enum class Color(
-    val r: Int,
-    val g: Int,
-    val b: Int
-) {
-    RED(255, 0, 0),
-    ORANGE(255, 165, 0),
-    YELLOW(255, 255, 0),
-    GREEN(0, 255, 0),
-    BLUE(0, 0, 255),
-    INDIGO(75, 0, 130),
-    VIOLET(238, 130, 238);
 
-    val rgb = (r * 256 + g) * 256 + b
-    fun printColor() = println("this is rgb : $rgb")
+
+interface Expr
+class Num(val value: Int) : Expr
+class Sum(val left: Expr, val right: Expr) : Expr
+class Sub(val left: Expr, val right: Expr) : Expr
+
+fun eval(e: Expr): Int {
+    if (e is Num) return e.value
+    if (e is Sum) return eval(e.left) + eval(e.right)
+    if (e is Sub) return eval(e.left) - eval(e.right)
+    throw IllegalArgumentException("Unknown expression")
 }
 
-/* Bir when expression’ının argümanı herhangi bir object olabilir. Branch koşullarıyla eşitlik açısından kontrol
-edilir. */
-fun mix(c1: Color, c2: Color) =
-    when (setOf(c1, c2)) {
-        /* Karıştırılabilen renk pair’larını enumerate eder. */
-        setOf(Color.RED, Color.YELLOW) -> Color.ORANGE
-        setOf(Color.YELLOW,Color.BLUE) -> Color.GREEN
-        setOf(Color.BLUE,Color.VIOLET) -> Color.INDIGO
-        /* Diğer branch’lerin hiçbiri eşleşmediğinde çalıştırılır. */
-        else -> throw IllegalArgumentException("Dirty Color")
-    }
-
 fun main() {
-    println(mix(Color.YELLOW, Color.RED))
+    // (1+2) + 4
+    val result = eval(
+        Sum(
+            Sum(Num(1), Num(2)),
+            Num(4)
+        )
+    )
+    println(result) // 7
 }
