dfc69aa071431db17698018b2c14d928a54910a6
Cihangir Kuce <developer@Developer-machine.local>
2025-12-27 23:39:25 +0300

    3.2.3 md dosyasi eklendi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,23 +1,7 @@
-import java.io.BufferedReader
-import java.io.StringReader
+import kotlin.math.E
+import kotlin.math.PI
+import kotlin.math.max
 
 fun main() {
-    val list = listOf(1, 2, 3)
-    println(joinToString(list)) // [1, 2, 3]
-    println(joinToString(list, seperator = "$ ")) // [1$ 2$ 3]
-}
-
-fun <T> joinToString(
-    collection: Collection<T>,
-    seperator: String = ", ",
-    prefix: String = "[",
-    postfix: String = "]"
-): String {
-    val result = StringBuilder(prefix)
-    for ((index, element) in collection.withIndex()) {
-        if (index > 0) result.append(seperator)
-        result.append(element)
-    }
-    result.append(postfix)
-    return result.toString()
+    println(max(PI, E)) // 3.141592653589793
 }
