cae441bf5eda3f9b461075547bf02f5ec23dbcbf
Cihangir Kuce <developer@Developer-machine.local>
2025-12-27 21:47:35 +0300

    2.5.2 md file olarak kaydedildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -3,17 +3,18 @@ import java.io.StringReader
 
 fun main() {
     val reader = BufferedReader(StringReader("11"))
-    println(readNumber(reader)) // 11
+    val reader2 = BufferedReader(StringReader("not a number"))
+    readNumber(reader) // 11
+    readNumber(reader2) // hiçbir şey yazdırılmaz
 }
 
-fun readNumber(reader: BufferedReader): Int? {
-    try {
-        val line = reader.readLine()
-        return Integer.parseInt(line)
-    } catch (e: NumberFormatException) {
-        return null
-    } finally {
-        reader.close()
+fun readNumber(reader : BufferedReader) {
+    // Exception oluşmadığında bu value kullanılır.
+    val number = try {
+        Integer.parseInt(reader.readLine())
+    } catch (e : NumberFormatException){
+        // Exception durumunda null value kullanılır.
+        null
     }
+    println(number)
 }
-
