8925174a34f597412ead5233f76e87d3ab03489e
Cihangir Kuce <developer@Developer-machine.local>
2025-12-28 21:16:26 +0300

    3.4.3 md haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,3 +1,13 @@
-fun main(args: Array<String>) {
-    val list = listOf(*args) // Spread operator, array’in contents’ini unpack eder.
+fun main() {
+    val list = listOf(
+        Person("Alice", 30),
+        Person("Bob", 25)
+    )
+    for((name,age) in list){
+        println("Name : $name, Age : $age")
+    }
+    // Name : Alice, Age : 30
+    // Name : Bob, Age : 25
 }
+
+data class Person(val name: String, val age: Int)
