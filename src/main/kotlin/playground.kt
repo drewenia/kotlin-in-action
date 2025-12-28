d191c6f5027e9d3d59f2c3f6c718059c65f8d0f9
Cihangir Kuce <developer@Developer-machine.local>
2025-12-28 18:35:10 +0300

    3.3.4 md haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,25 +1,15 @@
 fun main() {
-    // compile olmaz
-    // listOf(1,2,3).join()
+    val view : View = Button()
+    view.showoff() // I am a view
 }
 
-fun <T> Collection<T>.joinToString(
-    seperator: String = ", ",
-    prefix: String = "(",
-    postfix: String = ")"
-): String {
-    val result = StringBuilder(prefix)
-    // this burada Collection'dır
-    for ((index, element) in this.withIndex()) {
-        if (index > 0) result.append(seperator)
-        result.append(element)
-    }
-    result.append(postfix)
-    return result.toString()
+open class View {
+    open fun click() = println("View clicked")
 }
 
-fun Collection<String>.join(
-    seperator: String = "..",
-    prefix: String = "-",
-    postfix: String = "£"
-) = joinToString(seperator,prefix,postfix)
+class Button : View(){
+    override fun click() = println("Button clicked")
+}
+
+fun View.showoff() = println("I am a view")
+fun Button.showOff() = println("I am a button")
