36880836e3a9352761007c897b7aed3ad9b5837e
Cihangir Kuce <developer@Developer-machine.local>
2025-12-28 15:15:49 +0300

    3.3.3 md dosyasi eklendi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,8 +1,25 @@
 fun main() {
-    val lastChar = "Hello".lastChar()
-    println(lastChar) // o
+    // compile olmaz
+    // listOf(1,2,3).join()
 }
 
-// Burada String receiver type
-// this -> Receiver object (Yani String)
-fun String.lastChar() : Char = get(length -1)
+fun <T> Collection<T>.joinToString(
+    seperator: String = ", ",
+    prefix: String = "(",
+    postfix: String = ")"
+): String {
+    val result = StringBuilder(prefix)
+    // this burada Collection'dır
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(seperator)
+        result.append(element)
+    }
+    result.append(postfix)
+    return result.toString()
+}
+
+fun Collection<String>.join(
+    seperator: String = "..",
+    prefix: String = "-",
+    postfix: String = "£"
+) = joinToString(seperator,prefix,postfix)
