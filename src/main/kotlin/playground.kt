d0bc083c1c8f10ec98993cdd053cee237221cf8e
Cihangir Kuce <developer@Developer-machine.local>
2025-12-29 21:57:37 +0300

    4.1.5 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,42 +1,18 @@
 import java.io.Serializable
 
 fun main() {
-    val button = Button()
-    button.text = "OK"
-    button.enabled = false
-
-    val savedState = button.getCurrentState()
-
-    // state bozuluyor
-    button.text = "Cancel"
-    button.enabled = true
-
-    // geri yükleme
-    button.restoreState(savedState)
-
-    println(button.text) // OK
-    println(button.enabled) // false
+    val toggleable : Toggleable = LightSwitch()
+    toggleable.toggle() // Lights
 }
 
-interface State : Serializable
-
-interface View {
-    fun getCurrentState(): State
-    fun restoreState(state: State)
+sealed interface Toggleable{
+    fun toggle()
 }
 
-class Button : View {
-    var text: String = ""
-    var enabled: Boolean = true
-    override fun getCurrentState(): State = ButtonState(text, enabled)
+class LightSwitch : Toggleable {
+    override fun toggle() = println("Lights")
+}
 
-    override fun restoreState(state: State) {
-        // buradaki as Gelen State’i ButtonState olarak daraltır. Çünkü sadece Button kendi state’ini anlayabilir
-        val buttonState = state as ButtonState
-        text = buttonState.text
-        enabled = buttonState.enabled
-    }
-
-    // State = o anki durumun kopyası
-    class ButtonState(val text: String, val enabled: Boolean) : State
+class Camera : Toggleable {
+    override fun toggle() = println("Camera")
 }
