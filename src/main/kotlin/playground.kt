529f996f8a4b3f7651973bcef3cc3d29cfcc249a
Cihangir Kuce <developer@Developer-machine.local>
2025-12-29 21:38:25 +0300

    4.1.4 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,16 +1,42 @@
+import java.io.Serializable
+
 fun main() {
+    val button = Button()
+    button.text = "OK"
+    button.enabled = false
 
+    val savedState = button.getCurrentState()
+
+    // state bozuluyor
+    button.text = "Cancel"
+    button.enabled = true
+
+    // geri yükleme
+    button.restoreState(savedState)
+
+    println(button.text) // OK
+    println(button.enabled) // false
 }
 
-internal open class TalkativeButton{
-    private fun yell() = println("Hey!")
-    protected fun whisper() = println("Let's talk!")
+interface State : Serializable
+
+interface View {
+    fun getCurrentState(): State
+    fun restoreState(state: State)
 }
 
-// Hata: public bir member, internal receiver type TalkativeButton’ı expose ediyor (açığa çıkarıyor)
-/*
-fun TalkativeButton.giveSpeech(){
-    yell() // Hata: yell’e erişilemez; TalkativeButton içinde private’tır
-    whisper() // Hata: whisper’a erişilemez; TalkativeButton içinde protected’tır
+class Button : View {
+    var text: String = ""
+    var enabled: Boolean = true
+    override fun getCurrentState(): State = ButtonState(text, enabled)
+
+    override fun restoreState(state: State) {
+        // buradaki as Gelen State’i ButtonState olarak daraltır. Çünkü sadece Button kendi state’ini anlayabilir
+        val buttonState = state as ButtonState
+        text = buttonState.text
+        enabled = buttonState.enabled
+    }
+
+    // State = o anki durumun kopyası
+    class ButtonState(val text: String, val enabled: Boolean) : State
 }
-*/
