7896ddffcc14b914581c54f4a56cbfd27f612baa
Cihangir Kuce <developer@Developer-machine.local>
2025-12-29 20:14:16 +0300

    4.1.2 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,31 +1,12 @@
 fun main() {
-    val button = Button()
-
-    button.showOff()
-    // I am clickable
-    // I am focusable
-
-    button.setFocus(false)
-    // I lost focus
+    val employee : Employee = Developer()
+    print(employee.salary) // 60000
 }
 
-interface Clickable {
-    fun click()
-    fun showOff() = println("I am clickable")
+abstract class Employee{
+    abstract val salary : Int
 }
 
-interface Focusable {
-    fun setFocus(b: Boolean) = println("I ${if (b) "got" else "lost"} focus")
-    fun showOff() = println("I am focusable")
-}
-
-class Button : Clickable, Focusable {
-    override fun click() = println("I was clicked")
-    /* Aynı member için birden fazla implementation inherit ediliyorsa, açık bir implementation sağlamanız gerekir. */
-    override fun showOff() {
-        /* Burada, angle brackets içindeki supertype adıyla nitelendirilen super, call etmek istediğiniz method’un hangi
-        parent’a ait olduğunu belirtir. */
-        super<Clickable>.showOff()
-        super<Focusable>.showOff()
-    }
+class Developer : Employee(){
+    override val salary : Int = 60_000
 }
