1415852c9dcb6875d6a1d803f352103fffa6fa26
Cihangir Kuce <developer@Developer-machine.local>
2025-12-30 19:59:03 +0300

    4.2.4 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,39 +1,6 @@
 import java.net.URI
 
 fun main() {
-    val privateUser = PrivateUser("Ocean")
-    println(privateUser.nickname) // Ocean
-
-    val subscribingUser = SubscribingUser("egedata@gmail.com")
-    println(subscribingUser.nickname) // egedata
-
-    val socialUser = SocialUser(14)
-    println(socialUser.nickname) // kodee14
 
 }
 
-interface User {
-    val nickname: String
-}
-
-class PrivateUser(override val nickname: String) : User
-
-class SubscribingUser(val email: String) : User {
-    override val nickname: String
-        get() = email.substringBefore('@')
-}
-
-class SocialUser(accountId: Int) : User{
-    override val nickname: String = getNameFromSocialNetwork(accountId)
-}
-
-fun getNameFromSocialNetwork(accountId : Int) =
-    "kodee$accountId"
-
-interface EmailUser {
-    val email : String
-
-    // Property’nin bir backing field’ı yoktur: result value her erişimde hesaplanır.
-    val nickname : String
-        get() = email.substringBefore('@')
-}
