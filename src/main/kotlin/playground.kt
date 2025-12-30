8277c373e5be876b8af26b10d59f469745472ddd
Cihangir Kuce <developer@Developer-machine.local>
2025-12-30 20:33:41 +0300

    4.3 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,21 +1,14 @@
 import java.net.URI
 
 fun main() {
-    val lc = LengthCounter()
-    lc.addWord("Hello, World!")
-    println(lc.counter) // 13
 
-    // Error üretir çünkü set private halde
-    // lc.counter = 0
 }
 
-class LengthCounter {
-    var counter: Int = 0
-        // Bu property’yi class dışında değiştiremezsiniz.
-        private set
+class PersonTrivial(val name: String, val age: Int)
 
-    fun addWord(word: String) {
-        counter += word.length
-    }
+class PersonNonTrivial(val name: String, val birthYear: Int) {
+    val age : Int = 2025 - birthYear
 }
 
+// name için Otomatik getter/setter üretir
+class Person(var name : String)
