2f53051575e96c209a2bd63f554326cf4adb2048
Cihangir Kuce <developer@Developer-machine.local>
2025-12-30 20:13:23 +0300

    4.2.5 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,6 +1,21 @@
 import java.net.URI
 
 fun main() {
+    val lc = LengthCounter()
+    lc.addWord("Hello, World!")
+    println(lc.counter) // 13
 
+    // Error üretir çünkü set private halde
+    // lc.counter = 0
+}
+
+class LengthCounter {
+    var counter: Int = 0
+        // Bu property’yi class dışında değiştiremezsiniz.
+        private set
+
+    fun addWord(word: String) {
+        counter += word.length
+    }
 }
 
