d8e7f3dce11609809cf839301b62814aff16e4cc
Cihangir Kuce <developer@Developer-machine.local>
2025-12-30 22:40:43 +0300

    4.3.3 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,7 +1,22 @@
 fun main() {
-    val alice = Customer("Alice",11700)
-    val bob = alice.copy(name = "Bob")
-    println(bob) // Customer(name=Bob, postalCode=11700)
+    val countingSet = CountingSet<Int>()
+    countingSet.addAll(listOf(1, 2, 4, 4, 2))
+    println("Added ${countingSet.objectsAdded} objects, ${countingSet.size} uniques")
+    // Added 5 objects, 3 uniques
 }
 
-data class Customer(val name : String,val postalCode: Int)
+class CountingSet<T>(
+    private val innerSet: MutableCollection<T> = hashSetOf()
+) : MutableCollection<T> by innerSet {
+    var objectsAdded: Int = 0
+
+    override fun add(element: T): Boolean {
+        objectsAdded++
+        return innerSet.add(element)
+    }
+
+    override fun addAll(elements: Collection<T>): Boolean {
+        objectsAdded += elements.size
+        return innerSet.addAll(elements)
+    }
+}
