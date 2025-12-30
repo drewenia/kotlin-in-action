7c4c2a76bc2f7159265ca5f8b37ce4e0103e82a7
Cihangir Kuce <developer@Developer-machine.local>
2025-12-30 12:03:47 +0300

    4.2.1 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,18 +1,8 @@
-import java.io.Serializable
-
 fun main() {
-    val toggleable : Toggleable = LightSwitch()
-    toggleable.toggle() // Lights
+
 }
 
-sealed interface Toggleable{
-    fun toggle()
-}
+open class User(val nickname : String)
 
-class LightSwitch : Toggleable {
-    override fun toggle() = println("Lights")
-}
+class SocialUser(nickname: String) : User(nickname)
 
-class Camera : Toggleable {
-    override fun toggle() = println("Camera")
-}
