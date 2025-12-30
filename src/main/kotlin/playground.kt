75cc71092c8aba66dd62ec2150200814820629f7
Cihangir Kuce <developer@Developer-machine.local>
2025-12-30 15:08:55 +0300

    4.2.3 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,22 +1,39 @@
 import java.net.URI
 
 fun main() {
+    val privateUser = PrivateUser("Ocean")
+    println(privateUser.nickname) // Ocean
+
+    val subscribingUser = SubscribingUser("egedata@gmail.com")
+    println(subscribingUser.nickname) // egedata
+
+    val socialUser = SocialUser(14)
+    println(socialUser.nickname) // kodee14
 
 }
 
-open class Downloader {
-    constructor(url : String?){
-        // some code
-    }
-
-    constructor(uri : URI?){
-        // some code
-    }
+interface User {
+    val nickname: String
 }
 
-class MyDownloader : Downloader {
-    // this Class’ın başka bir constructor’ına delegation yapar
-    constructor(url : String?) : this(URI(url))
-    constructor(uri : URI?) : super(uri)
+class PrivateUser(override val nickname: String) : User
+
+class SubscribingUser(val email: String) : User {
+    override val nickname: String
+        get() = email.substringBefore('@')
 }
 
+class SocialUser(accountId: Int) : User{
+    override val nickname: String = getNameFromSocialNetwork(accountId)
+}
+
+fun getNameFromSocialNetwork(accountId : Int) =
+    "kodee$accountId"
+
+interface EmailUser {
+    val email : String
+
+    // Property’nin bir backing field’ı yoktur: result value her erişimde hesaplanır.
+    val nickname : String
+        get() = email.substringBefore('@')
+}
