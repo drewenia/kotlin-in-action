46eb6285c6f605a1326bb8dadbbc972205a80ed4
Cihangir Kuce <developer@Developer-machine.local>
2025-12-31 22:27:13 +0300

    5.1.2 md haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -2,8 +2,15 @@ import javax.swing.text.View
 import kotlin.math.exp
 
 fun main() {
+    val list = listOf(
+        Person("Alice", 30),
+        Person("Bob", 40)
+    )
+    println(findTheOldest(list)) // Person(name=Bob, age=40)
 }
 
+fun findTheOldest(peoples: List<Person>): Person? {
+    return peoples.maxByOrNull(Person::age)
+}
 
-
-
+data class Person(val name: String, val age: Int)
