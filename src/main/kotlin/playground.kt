6f676d7eeb3be4e742df6bca0c959955d77a277e
Cihangir Kuce <developer@Developer-machine.local>
2025-12-31 22:05:01 +0300

    5.1.1 md haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,21 +1,9 @@
+import javax.swing.text.View
 import kotlin.math.exp
 
 fun main() {
-    transfer(UserId(5), UserId(4), Amount(100))
 }
 
-@JvmInline
-value class UserId(val value: Int)
-
-@JvmInline
-value class Amount(val amount: Int)
-
-fun transfer(from: UserId, to: UserId, amount: Amount) {}
 
 
-interface MouseListener {
-    fun onEnter()
-    fun onClick()
-}
 
-class Button(private val listener: MouseListener)
