0ba127a2d80e8f86c14153117853214d3e1af9c7
Cihangir Kuce <developer@Developer-machine.local>
2025-12-31 20:31:46 +0300

    4.4.4 md dosyasi haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,31 +1,20 @@
-import ch04.Person443v2
-import kotlin.random.Random
-
 fun main() {
-    val json = """{ "name": "John Doe" }"""
-    val person = Person.fromJSON(json)
-    println("First name : ${person.firstName}, Last name : ${person.lastName}")
-    // First name : John, Last name : Doe
+    var clickCount = 0
+    Button(object : MouseListener{
+        override fun onEnter() {
+            TODO("Not yet implemented")
+        }
+
+        override fun onClick() {
+            clickCount++
+        }
+    })
 }
 
-// business logic module
-class Person(val firstName : String, val lastName : String){
-    // Boş bir companion object declare eder
-    companion object
+interface MouseListener {
+    fun onEnter()
+    fun onClick()
 }
 
-// client/server communication module
-fun Person.Companion.fromJSON(json : String) : Person{
-    val fullName = json
-        .substringAfter("\"name\"")
-        .substringAfter(":")
-        .substringAfter("\"")
-        .substringBefore("\"")
+class Button(private val listener: MouseListener)
 
-    val parts = fullName.split(" ", limit = 2)
-
-    val firstName = parts.getOrElse(0) { "" }
-    val lastName = parts.getOrElse(1) { "" }
-
-    return Person(firstName, lastName)
-}
