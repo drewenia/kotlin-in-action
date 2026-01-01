ec6a8d6a167a3f346785decdcb770ec7228972e9
Cihangir Kuce <developer@Developer-machine.local>
2026-01-01 21:24:37 +0300

    5.1.4 md haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,16 +1,17 @@
+
 fun main() {
-    println(getSum(1, 4)) // 5
+
 }
 
-fun getSum(x: Int, y: Int): Int {
-    val sum = { x: Int, y: Int ->
-        println("Computing the sum of $x and $y...")
-        x + y
-    }
-
-    return sum(x, y)
+class Button{
+    fun onClick() = println("click")
 }
 
-data class Person(val name: String, val age: Int)
+fun tryToCountButtonClicks(button : Button) : Int{
+    var clicks = 0
+    button.onClick() {clicks++}
+    return clicks
+}
+
 
 
