754e0251c9865552bd2a9bed7f53d00739a37d82
Cihangir Kuce <developer@Developer-machine.local>
2026-01-03 23:04:01 +0300

    5.4.1 md haline getirildi

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,24 +1,12 @@
 fun main() {
-    val simpleLambda : (String) -> Unit = {println(it)}
-
-    // 1. Durum: Otomatik Dönüşüm (Senin yaptığın)
-    // Derleyici "arka planda" SAM Constructor kullanır.
-    consumeHello(simpleLambda) // Hello
-
-    // 2. Durum: Manuel (Explicit) Dönüşüm
-    val explicit = StringConsumer(simpleLambda)
-    consumeHello(explicit) // Hello
+    print(alphabet())
+    // abcdefghijklmnopqrstuvwxyz
 }
 
-fun interface StringConsumer {
-    fun consume(s: String)
-}
-
-fun consumeHello(s: StringConsumer) {
-    s.consume("Hello")
-}
-
-fun consumeHelloFunctional(t: (String) -> Unit) {
-    t("Hello")
+fun alphabet() : String = with(StringBuilder()){
+    for (letter in 'a'..'z'){
+        append(letter)
+    }
+    toString()
 }
 
