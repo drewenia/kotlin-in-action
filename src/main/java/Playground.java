5dfea4ae399bea501be79a1f5971374ef672a3db
Cihangir Kuce <developer@Developer-machine.local>
2026-01-03 20:56:41 +0300

    5.2.2 md olarak kaydedildi

--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -1,16 +1,15 @@
 import javax.swing.text.View;
 
 public class Playground {
-}
-
-// Java Interface (Sadece bir tane abstract metod içeriyor -> SAM)
-interface OnClickListener{
-    void onClick(View v);
-}
-
-// Java Class (Bu interface'i parametre olarak alan bir metod içeriyor)
-class Button {
-    public void setOnClickListener(OnClickListener listener){
-        // Tıklama olduğunda listener.onClick(this) çağrılır
+    public static void main(String[] args) {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                // the logic
+            }
+        });
     }
 }
+
+
+
--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,5 +1,9 @@
 fun main() {
-
+    println(isAnswer)
 }
 
+fun interface Predicate<T> {
+    fun accept(element: T): Boolean
+}
 
+val isAnswer = Predicate<Int> { i -> i == 42 }
