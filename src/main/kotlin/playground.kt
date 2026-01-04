89565d8b214084c2f74212673c655f69b2bb3f6e
Cihangir Kuce <developer@Developer-machine.local>
2026-01-04 22:13:31 +0300

    6.1.2 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,17 +1,11 @@
 fun main() {
-    val numbers = mapOf(
-        0 to "Zero",
-        1 to "One",
-        2 to "Two"
-    )
+    val words = listOf("Kotlin", "öğrenmek", "keyiflidir")
 
-    val filterKeys = numbers
-        .filterKeys { it > 1 }
-    println(filterKeys) // {2=Two}
-
-    val mapKey = numbers
-        .mapKeys { "ID: ${it.key}"}
-    println(mapKey) // {ID: 0=Zero, ID: 1=One, ID: 2=Two}
+    val sentenceProgress = words.runningFold("Cümle") { acc, word ->
+        "$acc $word"
+    }
+    println(sentenceProgress)
+    // [Cümle, Cümle Kotlin, Cümle Kotlin öğrenmek, Cümle Kotlin öğrenmek keyiflidir]
 }
 
 data class Person(val name: String, val age: Int)
