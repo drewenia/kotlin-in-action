da3b63dc4d81f6530c9c4d3de80918d2484d382e
Cihangir Kuce <developer@Developer-machine.local>
2026-01-05 19:10:20 +0300

    6.1.7 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,20 +1,10 @@
 fun main() {
-    val people = listOf(
-        Person("Joe", 22),
-        Person("Mary", 31),
-        Person("Jamie", 22)
-    )
+    val names = mutableListOf("Martin", "Samuel")
+    names.replaceAll { it.uppercase() }
+    println(names) // [MARTIN, SAMUEL]
 
-    val personToAge = people.associateWith { it.age }
-    println(personToAge)
-    // Joe ve Jamie aynı yaştalar
-    // {Person(name=Joe, age=22)=22, Person(name=Mary, age=31)=31, Person(name=Jamie, age=22)=22}
-
-    // key olarak kullanılmaktadır …
-    val ageToPerson = people.associateBy { it.age }
-    println(ageToPerson)
-    // dolayısıyla map’te yalnızca ikincisi görünür.
-    // {22=Person(name=Jamie, age=22), 31=Person(name=Mary, age=31)}
+    names.fill("Redacted")
+    println(names) // [Redacted, Redacted]
 }
 
 data class Person(val name: String, val age: Int)
