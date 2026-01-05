c46588f1e50aeb8a25ad8e7e2fc9d55f3fa8073b
Cihangir Kuce <developer@Developer-machine.local>
2026-01-05 13:25:35 +0300

    6.1.4 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,14 +1,16 @@
 fun main() {
     val people = listOf(
-        Person("Derek", 31),
-        Person("Jim", 24),
+        Person("Derek", 26),
+        Person("Jim", 29),
         Person("Ari", 21),
         Person("David", 43),
     )
 
     val canBeInClub27 = { p: Person -> p.age <= 27 }
-    val find = people.find(canBeInClub27)
-    println(find) // Person(name=Jim, age=24)
+
+    val (comeIn, stayOut) = people.partition(canBeInClub27)
+    println(comeIn) // [Person(name=Derek, age=26), Person(name=Ari, age=21)]
+    println(stayOut) // [Person(name=Jim, age=29), Person(name=David, age=43)]
 }
 
 data class Person(val name: String, val age: Int)
