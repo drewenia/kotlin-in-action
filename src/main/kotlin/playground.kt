2713c7a986446acc31cd287f8540d48f5852e4ed
Cihangir Kuce <developer@Developer-machine.local>
2026-01-06 10:29:40 +0300

    6.1.9 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,7 +1,10 @@
 fun main() {
-    val blankName = " "
-    println(blankName.ifEmpty { "(unnamed)" }) //
-    println(blankName.ifBlank { "(unnamed)" }) // unnamed
+    val temperatures = listOf(27.7, 29.8, 22.0, 35.5, 19.1)
+    val chunked = temperatures.chunked(2)
+    println(chunked) // [[27.7, 29.8], [22.0, 35.5], [19.1]]
+
+    val summed = temperatures.chunked(2) { it.sum() }
+    println(summed) // [57.5, 57.5, 19.1]
 }
 
 data class Person(val name: String, val age: Int)
