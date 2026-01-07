c4ef58930fe5469fb2531af6df29de962ed4b4ef
Cihangir Kuce <developer@Developer-machine.local>
2026-01-07 22:27:00 +0300

    7.5 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -2,19 +2,29 @@ import java.io.File
 
 class Address(val streetAddress: String, val zipCode: Int, val city: String, val country: String)
 
-class Company(val name : String, val address : Address?)
+class Company(val name: String, val address: Address?)
 
-class Person(val name : String, val company : Company?)
+class Person(val name: String, val company: Company?)
 
-fun Person.countryName() : String {
-    // Birden fazla safe-call operator’ü zincir halinde kullanılabilir.
-    val country = this.company?.address?.country
-    return if (country != null) country else "Unknown"
+fun printShippingLabel(person: Person) {
+    val address = person.company?.address ?: throw IllegalArgumentException("No address")
+    with(address) {
+        println(streetAddress)
+        println("$zipCode $city, $country")
+    }
 }
 
 fun main() {
-    val person = Person("Dimitri",null)
-    println(person.countryName()) // Unknown
+    val address = Address("Elsestr. 47", 80687, "Munich", "Germany")
+    val jetbrains = Company("JetBrains", address)
+    val person = Person("Dimitri",jetbrains)
+    val person2 = Person("Alexey",null)
+
+    printShippingLabel(person)
+    // Elsestr. 47
+    // 80687 Munich, Germany
+
+    printShippingLabel(person2) // java.lang.IllegalArgumentException: No address
 }
 
 
