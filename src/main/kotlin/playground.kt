38d4dfec7914046c771c53904cd863b2e510edea
Cihangir Kuce <developer@Developer-machine.local>
2026-01-07 22:00:03 +0300

    7.4 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,16 +1,22 @@
 import java.io.File
 
-fun strLenSafe(s: String?): Int =
-    if (s != null) s.length else 0
+class Address(val streetAddress: String, val zipCode: Int, val city: String, val country: String)
+
+class Company(val name : String, val address : Address?)
+
+class Person(val name : String, val company : Company?)
+
+fun Person.countryName() : String {
+    // Birden fazla safe-call operator’ü zincir halinde kullanılabilir.
+    val country = this.company?.address?.country
+    return if (country != null) country else "Unknown"
+}
 
 fun main() {
-    val x : String? = null
-    val strLenSafe = strLenSafe(x)
-
-    println(strLenSafe) // 0
-    println(strLenSafe("abc")) // 3
+    val person = Person("Dimitri",null)
+    println(person.countryName()) // Unknown
 }
 
 
-data class Person(val name: String, val age: Int)
+// data class Person(val name: String, val age: Int)
 
