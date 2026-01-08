3d5f97eaa3753d42e0e1f5261c7fdb940df33ef3
Cihangir Kuce <developer@Developer-machine.local>
2026-01-08 20:20:55 +0300

    7.8 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,46 +1,16 @@
 fun main() {
-    val data = listOf(
-        "Kotlin",
-        "Java",
-        "Scala"
-    )
+    var email: String? = "yole@example.com"
 
-    val selectableTextList = SelectableTextList(data)
-    val copyRowAction = CopyRowAction(selectableTextList)
+    email?.let { sendEmailTo(it) }
+    //Sending to : yole@example.com
 
-    with(copyRowAction) {
-        if (isActionEnabled()) executeCopyRow()
-        else println("Buton pasif : seçili satır yok")
-        // Buton pasif : seçili satır yok
-    }
-
-    selectableTextList.selectedIndex = 1
-    with(copyRowAction){
-        if (isActionEnabled()){
-            println("Buton aktif islem gerceklestiriliyor")
-            executeCopyRow()
-            println("Basariyla kopyalandi ${selectableTextList.contents[selectableTextList.selectedIndex!!]}")
-            // Buton aktif islem gerceklestiriliyor
-            // Basariyla kopyalandi Java
-        }
-    }
+    email = null
+    email?.let { sendEmailTo(it) }
+    // hiçbirşey yapmaz
 }
 
-class SelectableTextList(
-    val contents: List<String>,
-    var selectedIndex: Int? = null
-)
+fun sendEmailTo(email: String) = println("Sending to : $email")
 
-class CopyRowAction(val list: SelectableTextList) {
-    fun isActionEnabled(): Boolean = list.selectedIndex != null
-
-    // executeCopyRow, yalnızca isActionEnabled true döndürdüğünde call edilir.
-    fun executeCopyRow() {
-        val index = list.selectedIndex!!
-        val value = list.contents[index]
-        // copy value to clipboard
-    }
-}
 
 // data class Person(val name: String, val age: Int)
 
