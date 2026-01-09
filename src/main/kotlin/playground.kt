4578e200a420325204233f4ad010c7015679c757
Cihangir Kuce <developer@Developer-machine.local>
2026-01-09 21:17:26 +0300

    7.9 refactored

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,24 +1,21 @@
-import kotlin.test.assertEquals
+class WeatherStation{
+    lateinit var latestReading : String
+
+    fun printReading(){
+        if (this::latestReading.isInitialized){
+            println("Latest reading : $latestReading")
+        } else {
+            println("No reading available")
+        }
+    }
+}
 
 fun main() {
-    val theName: String? = "Ari"
-    val theNumber: Int? = 7
+    val ws = WeatherStation()
 
-    val result = theName?.let { name ->
-        theNumber?.let { num -> "Hi $name, $num squared is ${num * num}" }
-    }
-
-    println(result) // Hi Ari, 7 squared is 49
-}
-
-open class Animal
-
-class Dog : Animal() {
-    fun bark() = println("Woof")
-}
-
-fun makeItBark(animal: Animal) {
-    (animal as? Dog)?.bark()
+    ws.printReading() // No reading available
+    ws.latestReading = "22°C, sunny"
+    ws.printReading() // Latest reading : 22°C, sunny
 }
 
 
