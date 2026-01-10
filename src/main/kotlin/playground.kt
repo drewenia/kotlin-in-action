f57d48e2a326ade7a00d1dc1b970b0bd934c0e77
Cihangir Kuce <developer@Developer-machine.local>
2026-01-11 02:42:48 +0300

    9.1.1 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,5 +1,14 @@
+operator fun Char.times(count: Int): String {
+    return toString().repeat(count)
+}
+
 fun main() {
+    val a = 'a'
+    println(a * 3) // aaa
 }
 
 data class Point(val x: Int, val y: Int)
+
+operator fun Point.times(scale: Double): Point =
+    Point((x * scale).toInt(), (y * scale).toInt())
 
