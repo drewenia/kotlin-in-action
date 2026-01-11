a93f4cd808885c0991ede3be6a975305b346fd7f
Cihangir Kuce <developer@Developer-machine.local>
2026-01-11 23:05:38 +0300

    9.5.3 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,42 +1,38 @@
+import kotlin.properties.Delegates
 import kotlin.reflect.KProperty
 
-class Email(val from: String, val to: String)
-
-class Person(val name: String) {
-    val emails by lazy {loadEmails(this)}
+fun interface Observer {
+    fun onChange(name: String, oldValue: Any?, newValue: Any?)
 }
 
-fun loadEmails(person: Person): List<Email> {
-    println("Load emails for ${person.name}")
-    return listOf(
-        Email("test@abc.com", "alice@jb.com")
-    )
-}
-
-class Delegate {
-    operator fun getValue(thisRef: Any?, property: KProperty<*>): String {
-        return "$thisRef, thank you for delegating '${property.name}' to me!"
-    }
-
-    operator fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
-        println("$value has been assigned to '${property.name}' in $thisRef.")
+open class Observable {
+    val observers = mutableListOf<Observer>()
+    fun notifyObservers(propName: String, oldValue: Any?, newValue: Any?) {
+        for (obs in observers) {
+            obs.onChange(propName, oldValue, newValue)
+        }
     }
 }
 
-class Example{
-    var p : String by Delegate()
+class Person(val name: String, age: Int, salary: Int) : Observable() {
+    private val onChange = { property: KProperty<*>, oldValue: Any?, newValue: Any? ->
+        notifyObservers(property.name, oldValue, newValue)
+    }
+
+    var age by Delegates.observable(age, onChange)
+    var salary by Delegates.observable(salary, onChange)
 }
 
-val lazyValue : String by lazy {
-    println("computed")
-    "Hello"
-}
 
 fun main() {
-    println(lazyValue)
-    // computed
-    // Hello
-
-    println(lazyValue)
-    // Hello
+    val p = Person("Seb", 28, 1000)
+    p.observers += Observer { propName, oldValue, newValue ->
+        println(
+            """
+            Property $propName changed from $oldValue to $newValue!
+            """.trimIndent()
+        )
+    }
+    p.age = 29 // Property age changed from 28 to 29!
+    p.salary = 1250 // Property salary changed from 1000 to 1250!
 }
