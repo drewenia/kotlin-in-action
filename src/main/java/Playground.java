846c1f821770c0aaab62a04ad443750b6d35431b
Cihangir Kuce <developer@Developer-machine.local>
2026-01-12 22:34:25 +0300

    9.3.4 refactored

--- src/main/java/Playground.java
+++ src/main/java/Playground.java
@@ -1,11 +1,6 @@
 public class Playground {
     public static void main(String[] args) {
-        Point p1 = new Point(10, 20);
-        Point p2 = new Point(5, 5);
 
-        Point result = p1.plus(p2);
-        System.out.println(result.getX()); // 15
-        System.out.println(result.getY()); // 25
     }
 }
 
--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,11 +1,41 @@
-operator fun Box.plus(extra: Int): Box {
-    return this.addWeight(extra)
-}
+import java.time.LocalDate
+
+operator fun ClosedRange<LocalDate>.iterator(): Iterator<LocalDate> =
+    // Bu object, LocalDate element’leri üzerinde bir Iterator implement eder.
+    object : Iterator<LocalDate> {
+        // start -> The minimum value in the range.
+        var current = start
+        override fun next(): LocalDate {
+            val thisDate = current
+
+            // Mevcut tarihi bir yıl artırır.
+            current = current.plusYears(1)
+
+            // Pre increment edilmiş tarihi döndürür.
+            return thisDate
+        }
+
+        // Tarihler için kullanılan compareTo convention’ına dikkat edin (current <= endInclusive)
+        // endInclusive -> The maximum value in the range (inclusive)
+        override fun hasNext(): Boolean = current <= endInclusive
+
+    }
 
 fun main() {
-    val myBox = Box(10)
-    val heavyBox = myBox + 5
-    println(heavyBox.weight) // 15
+    val birthDay = LocalDate.of(1976, 11, 29)
+    val today = LocalDate.now()
+
+    // birthDay'den 1 gün çıkart -> range today'e kadar
+    val monthsOff = birthDay.minusDays(1)..today
+
+    for (month in monthsOff){
+        println(month)
+        // 1976-11-28
+        // 1977-11-28
+        // 1978-11-28
+        // 1979-11-28
+        // 1980-11-28
+        // ...
+    }
 }
 
-data class Point(val x: Int, val y: Int)
