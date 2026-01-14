e666bf801833a51b7fa602037e79ba97be6cb1d3
Cihangir Kuce <developer@Developer-machine.local>
2026-01-14 19:36:27 +0300

    9.5.4 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,16 +1,13 @@
+import java.lang.reflect.Type
 import kotlin.properties.Delegates
 
-var max: Int by Delegates.vetoable(0) { property, oldValue, newValue ->
-    newValue > oldValue
+class C {
+    var prop : Type by MyDelegate()
 }
 
+val c = C
+
 fun main() {
-    println(max) // 0
-    max = 5
-    println(max) // 5
-    max = 10
-    println(max) // 10
-    max = 9
-    println(max) // 10
+
 }
 
