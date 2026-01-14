a65fa5187560234e89eafb62b8c6f47112235db5
Cihangir Kuce <developer@Developer-machine.local>
2026-01-14 21:04:56 +0300

    9.5.5 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,13 +1,21 @@
 import java.lang.reflect.Type
 import kotlin.properties.Delegates
 
-class C {
-    var prop : Type by MyDelegate()
+class Population(var cities : Map<String,Int>){
+    val sanFrancisco by cities
+    val tallinn by cities
+    val kotlin by cities
 }
 
-val c = C
+val population = Population(
+    mapOf(
+        "sanFrancisco" to 864_716,
+        "tallinn" to 413_782,
+        "kotlin" to 43_005
+    )
+)
 
 fun main() {
-
+    println(population.sanFrancisco) // 864716
 }
 
