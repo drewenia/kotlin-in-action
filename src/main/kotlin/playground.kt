ee1f02a7de3d09b2de4e1a36e02fda58315003e6
Cihangir Kuce <developer@Developer-machine.local>
2026-01-16 22:14:43 +0300

    10.1.6 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,52 +1,30 @@
-data class Person(
-    val firstName: String,
-    val lastName: String,
-    val phoneNumber: String?
-)
-
-class ContactListFilters {
-    var prefix: String = ""
-    var onlyWithPhoneNumber: Boolean = false
-
-    fun getPredicate(): (Person) -> Boolean {
-        // Bir function döndüren bir function declare eder
-        val startsWithPrefix = { p: Person ->
-            p.firstName.startsWith(prefix) ||
-                    p.lastName.startsWith(prefix)
-        }
-        if (!onlyWithPhoneNumber) {
-            // Bir function type türünde bir variable döndürür
-            return startsWithPrefix
-        }
-        // Bu function’dan bir lambda döndürür
-        return { startsWithPrefix(it) && it.phoneNumber != null }
-    }
+enum class OS {
+    WINDOWS,
+    LINUX,
+    MACOS,
+    IOS,
+    ANDROID
 }
 
+data class SiteVisit(
+    val path: String,
+    val duration: Double,
+    val os: OS
+)
+
+val log = listOf(
+    SiteVisit("/", 34.0, OS.WINDOWS),
+    SiteVisit("/", 22.0, OS.MACOS),
+    SiteVisit("/login", 12.0, OS.WINDOWS),
+    SiteVisit("/signup", 8.0, OS.IOS),
+    SiteVisit("/", 16.3, OS.ANDROID)
+)
+
+fun List<SiteVisit>.averageDurationFor(predicate : (SiteVisit) -> Boolean) =
+    filter(predicate).map(SiteVisit::duration).average()
+
+
 fun main() {
-    val contacts = listOf(
-        Person("Dimitry", "Jemerov", "123-4567"),
-        Person("Swetlana", "Isakova", null)
-    )
-
-    val contactListFilters = ContactListFilters()
-    with(contactListFilters) {
-        prefix = "D"
-        onlyWithPhoneNumber = true
-    }
-
-    // contacts.filter(contactListFilters.getPredicate())
-    // getPredicate tarafından döndürülen function’ı filter’a argument olarak geçirir
-    val filtered = contacts.filter(contactListFilters.getPredicate())
-    println(filtered) // [Person(firstName=Dimitry, lastName=Jemerov, phoneNumber=123-4567)]
-
-    val contactListFilters2 = ContactListFilters();
-    with(contactListFilters2){
-        onlyWithPhoneNumber = false
-    }
-
-    val filtered2 = contacts.filter(contactListFilters2.getPredicate())
-    println(filtered2)
-    // [Person(firstName=Dimitry, lastName=Jemerov, phoneNumber=123-4567),
-    // Person(firstName=Swetlana, lastName=Isakova, phoneNumber=null)]
+    val averageDuration = log.averageDurationFor { it.os == OS.IOS && it.path == "/signup" }
+    println(averageDuration) // 8.0
 }
