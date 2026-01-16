adef36da936700beb9a5fd429642e3436e4e9a08
Cihangir Kuce <developer@Developer-machine.local>
2026-01-17 00:33:52 +0300

    10.2.1 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,30 +1,10 @@
-enum class OS {
-    WINDOWS,
-    LINUX,
-    MACOS,
-    IOS,
-    ANDROID
+inline fun debugLog(block : () -> Unit){
+    println("Log started")
+    block()
+    println("Log done!")
 }
-
-data class SiteVisit(
-    val path: String,
-    val duration: Double,
-    val os: OS
-)
-
-val log = listOf(
-    SiteVisit("/", 34.0, OS.WINDOWS),
-    SiteVisit("/", 22.0, OS.MACOS),
-    SiteVisit("/login", 12.0, OS.WINDOWS),
-    SiteVisit("/signup", 8.0, OS.IOS),
-    SiteVisit("/", 16.3, OS.ANDROID)
-)
-
-fun List<SiteVisit>.averageDurationFor(predicate : (SiteVisit) -> Boolean) =
-    filter(predicate).map(SiteVisit::duration).average()
-
-
 fun main() {
-    val averageDuration = log.averageDurationFor { it.os == OS.IOS && it.path == "/signup" }
-    println(averageDuration) // 8.0
+    println("Log started")
+    println("Action started")
+    println("Log done!")
 }
