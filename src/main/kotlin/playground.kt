92299695f7b853c744ffb984b4cda2118c551afd
Cihangir Kuce <developer@Developer-machine.local>
2026-01-16 11:34:01 +0300

    10.1.4 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,21 +1,33 @@
-import javax.swing.Action
-
-class Button(val text: String, val onClick: () -> Unit)
-
-fun logOut() {
-    println("Oturum kapatıldı, ana sayfaya yönlendiriliyor...")
+fun <T> Collection<T>.joinToString(
+    seperator: String = ", ",
+    prefix: String = "#",
+    postfix: String = "#",
+    // Nullable bir function type türünde bir parameter declare eder
+    transform: ((T) -> String)? = null
+): String {
+    val result = StringBuilder(prefix)
+    for ((index, element) in this.withIndex()) {
+        if (index > 0) result.append(seperator)
+        // Function’ı call etmek için safe-call syntax’ını kullanır
+        val str = transform?.invoke(element)
+            // Callback’in belirtilmediği durumu handle etmek için Elvis operator’ünü kullanır
+            ?: element.toString()
+        result.append(str)
+    }
+    result.append(postfix)
+    return result.toString()
 }
 
 fun main() {
-    val saveButton = Button("Save"){
-        println("Save to database")
-        println("Saved")
-    }
+    val letters = listOf("Alpha", "Beta")
 
-    saveButton.onClick()
-    // Save to database
-    // Saved
+    // Default conversion function’ı kullanır
+    println(letters.joinToString()) // #Alpha, Beta#
 
-    val logOutButton = Button("Logout",::logOut)
-    logOutButton.onClick() // Oturum kapatıldı, ana sayfaya yönlendiriliyor...
+    // Argument olarak bir lambda geçirir
+    println(letters.joinToString { it.lowercase() }) // #alpha, beta#
+
+    // Bir lambda dahil olmak üzere birden fazla argument geçirmek için named argument syntax’ını kullanır
+    println(letters.joinToString(seperator = "$", prefix = "∫", postfix = "∫", transform = { it.uppercase() }))
+    // ∫ALPHA$BETA∫
 }
