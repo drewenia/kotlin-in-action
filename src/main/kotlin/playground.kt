d15bffa0c291d14bf76e2fcaf1096392b1de60a1
Cihangir Kuce <developer@Developer-machine.local>
2026-01-17 22:14:26 +0300

    10.2.5 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,13 +1,11 @@
-import kotlin.io.path.fileVisitor
+import java.io.File
 
-data class Person(val name: String, val age: Int)
-
-val people = listOf(
-    Person("Alice", 29),
-    Person("Bob", 31),
-)
-
+fun saveConfig(config : String){
+    File("config.txt").bufferedWriter().use{writer ->
+        writer.write(config)
+    }
+}
 fun main() {
-    val filteredAndMapping = people.filter { it.age > 30 }.map(Person::name)
-    println(filteredAndMapping) // [Bob]
+    saveConfig("Theme=Dark\nFontSize=14")
+    println("Config saved successfully.")
 }
