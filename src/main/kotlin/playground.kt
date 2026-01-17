b213ef9bbd30aa100b2ee4370aad91004941a510
Cihangir Kuce <developer@Developer-machine.local>
2026-01-17 22:39:53 +0300

    10.3.2 completed

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,11 +1,27 @@
-import java.io.File
+data class Person(val name: String, val age: Int)
 
-fun saveConfig(config : String){
-    File("config.txt").bufferedWriter().use{writer ->
-        writer.write(config)
+val people = listOf(
+    Person("Alice", 29),
+    Person("Bob", 31)
+)
+
+fun lookForAlice(people: List<Person>) {
+    // Lambda expression’ı label’lar
+    people.forEach {
+        // return@label, bu label’a referans verir.
+        if (it.name != "Alice") return@forEach
+        // Bu satır yalnızca return execute edilmediğinde yazdırılır.
+        println("Found Alice")
     }
 }
+
 fun main() {
-    saveConfig("Theme=Dark\nFontSize=14")
-    println("Config saved successfully.")
+    // Bu lambda’nın implicit receiver’ına this@sb ile erişilir.
+    println(StringBuilder().apply sb@{
+        listOf(1, 2, 3).apply { // this, scope’daki en yakın implicit receiver’a referans verir.
+            // Tüm implicit receiver’lara erişilebilir; dıştaki olanlara explicit label’lar aracılığıyla.
+            this@sb.append(this.toString())
+        }
+    })
+    // [1, 2, 3]
 }
