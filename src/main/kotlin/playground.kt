d913bbce838dadf36d1d5a54e2da51d34dc018bd
Cihangir Kuce <developer@Developer-machine.local>
2026-01-22 22:51:44 +0300

    Covariant - Contravariant extra description added

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,18 +1,27 @@
-open class Animal
-class Cat : Animal()
-
-/* enumerateCats şunu şart koşuyor: "Bana bir fonksiyon ver, bu fonksiyon bir Cat alsın ve bana bir Number versin."
-Tipi: (Cat) -> Number */
-fun enumerateCats(f: (Cat) -> Number){
-    val myCat = Cat()
-    val result = f(myCat)
-    println("$result")
+open class Mammal(val name: String) {
+    fun eat() {}
+    fun sleep() {}
 }
 
-// Girdi: Bir Animal nesnesi. Çıktı bir int değeri. sembolik olarak 42 yazdım
-// Yani bu fonksiyonun tipi aslında şudur: (Animal) -> Int.
-fun Animal.getIndex() : Int = 42
+data class Sloth(val slothName: String, val isTwoFingered: Boolean) : Mammal(slothName)
 
-fun main(){
-    enumerateCats (Animal::getIndex)
+data class Panda(val pandaName: String) : Mammal(pandaName)
+
+fun main() {
+    // Burada ki list compiler tarafından infer edildiğinde List<Mammal> olur
+    val crewList = listOf(
+        Sloth("Jerry", false),
+        Sloth("Bae", true),
+        Sloth("Chrissy", false),
+        Panda("Jay"),
+        Panda("Peggy")
+    )
+
+    val compareByNames = Comparator{a : Mammal, b: Mammal ->
+        a.name.first().code - b.name.first().code
+    }
+
+    println(crewList.sortedWith(compareByNames))
+    /* [Sloth(slothName=Bae, isTwoFingered=true), Sloth(slothName=Chrissy, isTwoFingered=false),
+    Sloth(slothName=Jerry, isTwoFingered=false), Panda(pandaName=Jay), Panda(pandaName=Peggy)]*/
 }
