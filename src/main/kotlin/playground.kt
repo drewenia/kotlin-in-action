c96d5fd37adf3a59e29041e4a032ee12de7c1d2e
Cihangir Kuce <developer@Developer-machine.local>
2026-01-23 23:14:25 +0300

    11.3.6 added

--- src/main/kotlin/playground.kt
+++ src/main/kotlin/playground.kt
@@ -1,12 +1,40 @@
-fun <T> copyData(source: MutableList<T>, destination: MutableList<in T>) {
-    for (item in source) {
-        destination.add(item)
-    }
+import kotlin.reflect.KClass
+
+interface FieldValidator<in T> {
+    fun validate(input: T): Boolean
 }
 
+object DefaultStringValidator : FieldValidator<String> {
+    override fun validate(input: String) = input.isNotEmpty()
+}
+
+object DefaultIntValidator : FieldValidator<Int> {
+    override fun validate(input: Int) = input >= 0
+}
+
+object Validators {
+    // Öncekiyle aynı map’i kullanır, ancak artık ona dışarıdan access edemezsiniz.
+    private val validators = mutableMapOf<KClass<*>, FieldValidator<*>>()
+
+    // Validator’ın bir class ile karşılık geldiği durumda, map’e yalnızca doğru key–value pair’ları koyar.
+    fun <T : Any> registerValidator(
+        kClass: KClass<T>, fieldValidator: FieldValidator<T>
+    ) {
+        validators[kClass] = fieldValidator
+    }
+
+    // FieldValidator<T>’ye yapılan unchecked cast hakkındaki uyarıyı bastırır.
+    @Suppress("UNCHECKED_CAST")
+    operator fun <T : Any> get(kClass: KClass<T>): FieldValidator<T> =
+        validators[kClass] as? FieldValidator<T>
+            ?: throw IllegalArgumentException("No validator for ${kClass.simpleName}")
+}
+
+
 fun main() {
-    val list : MutableList<out Number> = mutableListOf()
-    // list.add(42)
-    // Error: Out-projected type 'MutableList<out Number>',
-    // 'fun add(element: E): Boolean' kullanımını yasaklar
+    Validators.registerValidator(String::class, DefaultStringValidator)
+    Validators.registerValidator(Int::class, DefaultIntValidator)
+
+    println(Validators[String::class].validate("Kotlin")) // true
+    println(Validators[Int::class].validate(42)) // true
 }
